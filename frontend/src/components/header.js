56170d549d79ba0a180343b154a75fd0e2f09311
Randy Ferrer <randfer@gmail.com>
2019-05-24 11:37:33 -0400

    Basic page layout

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,42 +1,40 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+import styled from 'styled-components';
+
+import { CentralColumn, Heading } from './styles';
+
+const HeaderStyle = styled.header`
+  background: rebeccapurple;
+`;
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+  <HeaderStyle>
+    <CentralColumn style={{ height: '100%' }}>
+      <Heading style={{ lineHeight: '70px' }}>
+        <h1>
+          <Link
+            to='/'
+            style={{
+              color: `white`,
+              textDecoration: `none`
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </Heading>
+    </CentralColumn>
+  </HeaderStyle>
+);
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  siteTitle: PropTypes.string
+};
 
 Header.defaultProps = {
-  siteTitle: ``,
-}
+  siteTitle: ``
+};
 
-export default Header
+export default Header;
