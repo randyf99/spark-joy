c336953f95778d33fc44678b81f6e67ebd47d7ba
Randy Ferrer <randfer@gmail.com>
2019-05-24 12:49:50 -0400

    Sketch out the widget builder

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 // import { Link } from 'gatsby';
+import { CentralColumn } from '../components/styles';
 
 import Layout from '../components/layout';
 // import Image from '../components/image';
 import SEO from '../components/seo';
-import { CentralColumn } from '../components/styles';
+
+import WidgetBuilder from '../components/WidgetBuilder';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title='Home' />
+    <SEO />
     <CentralColumn>
-      <p>this is body</p>
+      <p>Ask if it sparked joy.</p>
+      <WidgetBuilder />
     </CentralColumn>
   </Layout>
 );
