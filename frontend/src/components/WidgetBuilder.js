3d64fa96dde8db11e7b1b142a62ec2c1aeedffdb
Randy Ferrer <randfer@gmail.com>
2019-05-29 17:01:46 -0400

    Copy non-editable joy widget to clipboard

--- frontend/src/components/WidgetBuilder.js
+++ frontend/src/components/WidgetBuilder.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 import { Button } from 'rebass';
 
 import { Heading, Flex } from './styles';
 import { palette } from 'styled-tools';
+import { copyToClipboard } from '../utils';
 
 const Input = styled.input`
   border: 0;
@@ -36,32 +38,56 @@ const Layout = styled.div`
   width: 450px;
   height: 250px;
   display: grid;
-  grid-template-rows: 0.5fr 1fr 0.5fr;
+  grid-template-rows: 1fr 0.2fr;
+`;
+
+const WidgetLayout = styled.div`
+  width: 450px;
+  display: grid;
+  grid-template-rows: 0.3fr 0.7fr;
 `;
 
 const Question = styled(Heading)`
   text-align: center;
 `;
 
+const Widget = ({ editable, value, update }) => (
+  <WidgetLayout>
+    <Question h2>
+      Did this{' '}
+      {editable ? (
+        <Input
+          type='text'
+          value={value}
+          onChange={event => update(event.target.value)}
+        />
+      ) : (
+        value
+      )}{' '}
+      spark joy?
+    </Question>
+    <Flex row>
+      <RoundButton>👎</RoundButton>
+      <RoundButton>👍</RoundButton>
+    </Flex>
+  </WidgetLayout>
+);
+
 const WidgetBuilder = () => {
   const [typeOfJoy, setTypeOfJoy] = useState('');
 
+  function exportWidget() {
+    const el = document.createElement('div');
+    ReactDOM.render(<Widget value={typeOfJoy} />, el);
+    copyToClipboard(el.innerHTML);
+  }
+
   return (
     <Layout>
-      <Question h2>
-        Did this{' '}
-        <Input
-          type='text'
-          value={typeOfJoy}
-          onChange={event => setTypeOfJoy(event.target.value)}
-        />{' '}
-        spark joy?
-      </Question>
-      <Flex row>
-        <RoundButton>👎</RoundButton>
-        <RoundButton>👍</RoundButton>
-      </Flex>
-      <Button bg='primary'>Export</Button>
+      <Widget editable typeOfJoy={typeOfJoy} update={setTypeOfJoy} />
+      <Button bg='primary' onClick={exportWidget}>
+        Export
+      </Button>
     </Layout>
   );
 };
