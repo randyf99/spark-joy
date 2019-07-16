582c21e729891eb3978864b67520b37ab57989ff
Randy Ferrer <randfer@gmail.com>
2019-07-15 22:11:00 -0400

    Refactored form into separate files, added animation to enter text

--- frontend/src/pages/vote.js
+++ frontend/src/pages/vote.js
@@ -1,13 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useApolloClient } from 'react-apollo-hooks';
-import { Form, Field } from 'react-final-form';
-import { Button, Heading, Text, Box, Flex } from 'rebass';
 import styled from 'styled-components';
-import { fontSize, lineHeight, fontFamily } from 'styled-system';
-
 // import Image from '../components/image';
 import SEO from '../components/seo';
 import { WIDGET_VOTE_QUERY, SAVE_WIDGET_FEEDBACK_QUERY } from '../queries';
+import { FullScreenForm } from '../components/FullScreenForm';
 
 const Fullscreen = styled.div`
   display: grid;
@@ -16,31 +13,6 @@ const Fullscreen = styled.div`
   align-items: center;
   min-height: 100vh;
   text-align: center;
-`;
-
-const Input = styled('input')(
-  {
-    padding: '0.5rem 1rem',
-    width: '80%',
-    border: 0
-  },
-  fontSize,
-  lineHeight,
-  fontFamily
-);
-
-const InputBox = styled(Box)`
-  height: 60px;
-`;
-
-const ExplainerText = styled(Text)`
-  width: 80%;
-  text-align: left;
-  display: inline-block;
-`;
-
-const BulletText = styled(Text)`
-  display: inline;
 `;
 
 async function saveVote({ widgetId, voteType, apolloClient }) {
@@ -52,40 +24,6 @@ async function saveVote({ widgetId, voteType, apolloClient }) {
       thumbsdown: voteType === 'thumbsdown'
     }
   });
-}
-
-const InputComponent = props => (
-  <InputBox>
-    <BulletText fontSize={[1, 2, 3]}>{props.index + 1}. 👉</BulletText>
-    <Input
-      fontSize={[3, 4, 5]}
-      placeholder='Listen to your gut :)'
-      {...props.input}
-    />
-    {props.meta.active ? (
-      <ExplainerText fontSize={[0.5, 1, 1]}>
-        <strong>Enter</strong> to submit
-      </ExplainerText>
-    ) : null}
-  </InputBox>
-);
-
-function renderField({ index, id, label, type }) {
-  return (
-    <div key={id}>
-      <Heading fontSize={(4, 5, 6)}>
-        <label>{label}</label>
-      </Heading>
-      <br />
-      <Field
-        name={`field_${id}`}
-        component={InputComponent}
-        type='text'
-        initialValue=''
-        index={index}
-      />
-    </div>
-  );
 }
 
 // Final submission method
@@ -104,34 +42,20 @@ const VotePage = ({ pageContext }) => {
   const apolloClient = useApolloClient();
   const { widgetId, voteType, followupQuestions } = pageContext;
 
-  const [fieldIndex, setFieldIndex] = useState(0);
-
   useEffect(() => {
     saveVote({ widgetId, voteType, apolloClient });
   }, []);
 
   function onSubmit(values) {
-    if (fieldIndex >= followupQuestions.length - 1) {
-      console.log(values);
-    } else {
-      setFieldIndex(fieldIndex + 1);
-    }
+    console.log(values);
   }
 
   return (
     <Fullscreen>
       <SEO title='Thank You' />
-      <Form
-        // onSubmit={values => onSubmit({ widgetId, values, apolloClient })}
+      <FullScreenForm
         onSubmit={onSubmit}
-        render={({ handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            {renderField({
-              index: fieldIndex,
-              ...followupQuestions[fieldIndex]
-            })}
-          </form>
-        )}
+        followupQuestions={followupQuestions}
       />
     </Fullscreen>
   );
